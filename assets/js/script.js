f01286713c42e8bfc47de87df7f60fa5785fe50d
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2022-08-03 22:10:09 -0400

    hour -day with moment js finished

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // Current Day - Hour - moment.js //
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
+
+    
+})
