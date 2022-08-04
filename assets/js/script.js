56c6cdc706d056c3e13ff5aa4b714224f0822122
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2022-08-03 23:13:28 -0400

    base html - rows columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
-$(document).ready(function () {
+window.setInterval(function () {
     // Current Day - Hour - moment.js //
-    let NowMoment = moment().format("MMMM Do YYYY");
+    let NowMoment = moment().format('MMMM Do YYYY, h:mm:ss a');;
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
     let currentHour = moment().format("HH");
+}, 1000);
 
-    
-})
