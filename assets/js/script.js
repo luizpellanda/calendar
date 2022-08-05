5db8ad66f6fde201927f36af28213592b44fddfc
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2022-08-04 21:09:08 -0400

    finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 });  
     
     // moment.js //
-window.setInterval(function () {    
+    window.setInterval(function () {    
     let NowMoment = moment().format('MMMM Do YYYY, h:mm:ss a');;
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
