c4282c18875f826dc092911dd56abdd06a0920ed
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2022-08-04 21:00:10 -0400

    javascript added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
-window.setInterval(function () {
-    // Current Day - Hour - moment.js //
+$(document).ready(function () {
+
+      // clear content button
+    $("#clearSchedule").click(function (event) {
+      event.preventDefault;
+      $("textarea").val("");
+      localStorage.clear();
+    });
+  
+    //uses data from timeslots and compares it to current time
+    let currentHour = moment().format("HH");
+
+    $(".time-div").each(function () {
+      let timeDiv = $(this).attr("id").split("-")[1];
+      
+      if (currentHour == timeDiv) {
+        $(this).addClass("present");
+        $(this).children(".description").addClass("white-text");
+      } else if (currentHour < timeDiv) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else if (currentHour > timeDiv) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+      }
+    });
+  
+    //saves elements in local storage
+    $(".saveBtn").click(function (event) {
+      event.preventDefault();
+      let value = $(this).siblings(".time-block").val();
+      let time = $(this).parent().attr("id").split("-")[1];
+      localStorage.setItem(time, value);
+    });
+  
+    //retrieve elements from local storage
+    $("#hour-09 .time-block").val(localStorage.getItem("09"));
+    $("#hour-10 .time-block").val(localStorage.getItem("10"));
+    $("#hour-11 .time-block").val(localStorage.getItem("11"));
+    $("#hour-12 .time-block").val(localStorage.getItem("12"));
+    $("#hour-13 .time-block").val(localStorage.getItem("13"));
+    $("#hour-14 .time-block").val(localStorage.getItem("14"));
+    $("#hour-15 .time-block").val(localStorage.getItem("15"));
+    $("#hour-16 .time-block").val(localStorage.getItem("16"));
+    $("#hour-17 .time-block").val(localStorage.getItem("17"));
+
+});  
+    
+    // moment.js //
+window.setInterval(function () {    
     let NowMoment = moment().format('MMMM Do YYYY, h:mm:ss a');;
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = NowMoment;
